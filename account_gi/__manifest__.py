df92f345689dea6e9d77085fc3a827bea2ac4470
Mateo A Zabala G <mzabalagutierrez@gmail.com>
2018-11-27 17:54:01 -0600

    modificando roles

--- account_gi/__manifest__.py
+++ account_gi/__manifest__.py
@@ -18,7 +18,7 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base','account','account_cancel','partner_credit_limit','contacts','product','l10n_mx_edi','currency_rate_live'],
+    'depends': ['base','account','account_reports','account_cancel','partner_credit_limit','contacts','product','l10n_mx_edi','currency_rate_live'],
     'application': False,
 
     # always loaded
