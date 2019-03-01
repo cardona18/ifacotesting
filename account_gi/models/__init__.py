5bba4510212017eb6b5bdbcd50581462c6df76ec
Mateo A Zabala G <mzabalagutierrez@gmail.com>
2019-03-01 09:53:37 -0500

    Cambio para instalar account_gi

--- account_gi/models/__init__.py
+++ account_gi/models/__init__.py
@@ -11,8 +11,9 @@ from . import res_cancel_types
 #Migracion
 
 #Borrado en el .csv
+#hr_expense.access_uom_uom_hr_expense_user,product.uom.hr.expense.user,product.model_uom_uom,hr_expense.group_hr_expense_user,1,0,0,0
+#mrp.access_uom_uom_mrp_manager,product.uom mrp_manager,product.model_uom_uom,mrp.group_mrp_manager,1,0,0,0
+#sale.access_uom_uom_sale_manager,product.uom salemanager,product.model_uom_uom,sales_team.group_sale_manager,1,0,0,0
+#purchase.access_uom_uom_purchase_manager,product.uom purchase_manager,product.model_uom_uom,purchase.group_purchase_manager,1,0,0,0
+#stock.access_uom_uom_stock_manager,product.uom stock_manager,product.model_uom_uom,stock.group_stock_manager,1,0,0,0
 #,,,,,,,
-#stock.access_product_uom_categ_stock_manager,product.uom.categ stock_manager,product.model_product_uom_categ,stock.group_stock_manager,1,1,0,0
-#purchase.access_product_uom_categ_purchase_manager,product.uom.categ purchase_manager,product.model_product_uom_categ,purchase.group_purchase_manager,1,1,0,0
-#sale.access_product_uom_categ_sale_manager,product.uom.categ salemanager,product.model_product_uom_categ,sales_team.group_sale_manager,1,1,0,0
-#mrp.access_product_uom_categ_mrp_manager,product.uom.categ mrp_manager,product.model_product_uom_categ,mrp.group_mrp_manager,1,1,0,0
